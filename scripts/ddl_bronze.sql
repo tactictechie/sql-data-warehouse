97d4579be04f5d20d9d266bc0ff0e8734fb2b180
tactictechie <shivangsagwaliya@gmail.com>
2025-10-05 15:39:50 +0530

    Update ddl_bronze.sql

--- scripts/ddl_bronze.sql
+++ scripts/ddl_bronze.sql
@@ -44,7 +44,7 @@ CREATE TABLE bronze.crm_prd_info(
 	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
-	sls_order INT,
+	sls_order_dt INT,
 	sls_ship_dt INT,
 	sls_due_dt INT,
 	sls_sales INT,
